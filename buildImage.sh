4bf8d44abc9c07126bfbd846118141b5fddff4e4
olga <fedorova-olga.info@yandex.ru>
2021-03-17 19:58:23 +0300

    Update scripts and a description of the task

--- buildImage.sh
+++ buildImage.sh
@@ -1,12 +1,22 @@
 #1 /bin/bash
 set -e
 
+imageTag=$1
+if [ -z "$1" ]
+  then
+    echo No image tag provided. latest will be used
+    imageTag=latest
+fi
+
+repositoryName=739411955168.dkr.ecr.us-east-2.amazonaws.com/ci-cd-practise
+imageFullName=$repositoryName:$imageTag
+
 echo Main App STARTING building image...
 
 echo Main App creating jar...
 gradle bootJar
 
 echo Main App creating docker image...
-docker build -t ci-cd-practise .
+docker build -t $imageFullName .
 
 echo Main App FINISHED image has been built
--- pushImage.sh
+++ pushImage.sh
@@ -1,15 +1,22 @@
 #1 /bin/bash
 set -e
 
+imageTag=$1
+if [ -z "$1" ]
+  then
+    echo No image tag provided. latest will be used
+    imageTag=latest
+fi
+
+repositoryName=739411955168.dkr.ecr.us-east-2.amazonaws.com/ci-cd-practise
+imageFullName=$repositoryName:$imageTag
+
 echo Main App STARTING pushing image...
 
 echo Main App get login...
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 739411955168.dkr.ecr.us-east-1.amazonaws.com
-
-echo Main App tag application...
-docker tag ci-cd-practise:latest 739411955168.dkr.ecr.us-east-1.amazonaws.com/ci-cd-practise:latest
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 739411955168.dkr.ecr.us-east-2.amazonaws.com
 
 echo Main App push image...
-docker push 739411955168.dkr.ecr.us-east-1.amazonaws.com/ci-cd-practise:latest
+docker push $imageFullName
 
 echo Main App FINISHED image has been pushed
